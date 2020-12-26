16f0fd0e1389c9ad87cbcca8b0e159a720e0ed63
retrooper <retrooperdev@gmail.com>
2020-12-26 18:19:21 +0100

    Updated PacketPlayClientAbilities
    PacketPlayClientAbilities now supports 1.8-1.16.
    All fields except the flying field are optional as they don’t exist on 1.16 and above.
    The flying field exists on 1.8 and above.

--- src/main/java/cc/ghast/packet/wrapper/packet/play/client/PacketPlayClientAbilities.java
+++ src/main/java/cc/ghast/packet/wrapper/packet/play/client/PacketPlayClientAbilities.java
@@ -8,6 +8,7 @@ import cc.ghast.packet.wrapper.packet.Packet;
 import cc.ghast.packet.wrapper.packet.ReadableBuffer;
 import lombok.Getter;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Getter
@@ -15,21 +16,50 @@ public class PacketPlayClientAbilities extends Packet<ClientPacket> implements R
     public PacketPlayClientAbilities(UUID player, ProtocolVersion version) {
         super("PacketPLayInAbilities", player, version);
     }
+
+    private Optional<Boolean> invulnerable;
     private boolean flying;
+    private Optional<Boolean> allowedFlight;
+    private Optional<Boolean> creativeMode;
+
+    private Optional<Float> flySpeed;
+    private Optional<Float> walkSpeed;
 
     @Override
     public void read(ProtocolByteBuf byteBuf) {
+
         // These are under a bit mask, see https://stackoverflow.com/questions/31575691/what-is-a-bitmask-and-a-mask
         byte flags = byteBuf.readByte();
 
         /*
+            Creative Mode -> 0x01
             Flying -> 0x02
+            Allowed Flight -> 0x04
+            Invulnerable -> 0x08
          ------------------
             For those who don't understand why this works, basically we're reading binary: 0000 0001, 0000 0010
             The '&' symbol merges two bytes and UNIQUELY keeps the common bytes. If we take this into what's being done
             Here, basically we are placing a 1 on different scales. If the one is here, it's true. Simple yet complex.
 
         */
+
         this.flying = (flags & 0x02) > 0;
+        if(version.isOrBelow(ProtocolVersion.V1_15_2)) {
+            this.creativeMode = Optional.of((flags & 0x01) > 0);
+            this.allowedFlight = Optional.of((flags & 0x04) > 0);
+            this.invulnerable = Optional.of((flags & 0x08) > 0);
+
+            this.flySpeed = Optional.of(byteBuf.readFloat());
+            this.walkSpeed = Optional.of(byteBuf.readFloat());
+        }
+        else {
+            //Don't exist on 1.16+ :(
+            this.creativeMode = Optional.empty();
+            this.allowedFlight = Optional.empty();
+            this.invulnerable = Optional.empty();
+
+            this.flySpeed = Optional.empty();
+            this.walkSpeed = Optional.empty();
+        }
     }
 }
