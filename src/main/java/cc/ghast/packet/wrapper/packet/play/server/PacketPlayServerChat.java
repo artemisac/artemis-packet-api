8df013de1d3e33a52efd40258e7292558c0232c9
retrooper <retrooperdev@gmail.com>
2021-01-03 14:38:33 +0100

    PacketPlayServerChat created

--- src/main/java/cc/ghast/packet/wrapper/packet/play/server/PacketPlayServerChat.java
+++ src/main/java/cc/ghast/packet/wrapper/packet/play/server/PacketPlayServerChat.java
@@ -13,8 +13,57 @@ public class PacketPlayServerChat extends Packet<ServerPacket> implements Readab
         super("PacketPlayOutChat", player, version);
     }
 
+    private String text;
+    //1.7.10 clients don't send this setting...
+    private ChatMessageType type;
+
     @Override
     public void read(ProtocolByteBuf byteBuf) {
+        if (version.isBelow(ProtocolVersion.V1_8)) {
+            this.type = ChatMessageType.CHAT;
+        }
+        else {
+            byte position = byteBuf.readByte();
+            this.type = ChatMessageType.getChatMessageType(position);
+        }
 
+        final int stringBufArg;
+        if (version.isBelow(ProtocolVersion.V1_13)) {
+            stringBufArg = 32767;
+        }
+        else {
+            stringBufArg = 262144;
+        }
+        this.text = byteBuf.readStringBuf(stringBufArg);
     }
+
+    public enum ChatMessageType {
+        CHAT((byte)0),
+        SYSTEM((byte)1),
+        GAME_INFO((byte)2);
+
+        private final byte position;
+
+        ChatMessageType(byte position) {
+            this.position = position;
+        }
+
+        public byte getPosition() {
+            return this.position;
+        }
+
+        public static ChatMessageType getChatMessageType(final byte position) {
+            ChatMessageType[] values = values();
+            int length = values.length;
+
+            for (int i = 0; i < length; ++i) {
+                ChatMessageType type = values[i];
+                if (position == type.position) {
+                    return type;
+                }
+            }
+            return CHAT;
+        }
+    }
+
 }
