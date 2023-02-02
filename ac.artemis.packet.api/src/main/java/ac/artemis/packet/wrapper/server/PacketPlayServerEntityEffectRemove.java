6323cb30d62ccebcf5c494d3b96ad28e9b856bc0
Hydrogen <34059898+thehydrogen@users.noreply.github.com>
2023-02-02 17:47:18 +0100

    fix build error

--- ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerEntityEffectRemove.java
+++ ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerEntityEffectRemove.java
@@ -3,7 +3,4 @@ package ac.artemis.packet.wrapper.server;
 import ac.artemis.packet.wrapper.PacketServer;
 
 public interface PacketPlayServerEntityEffectRemove extends PacketServer {
-
-    int getEntityId();
-    byte getEffectId();
 }
