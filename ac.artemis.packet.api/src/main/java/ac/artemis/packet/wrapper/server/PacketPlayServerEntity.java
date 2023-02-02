54e5d2ddc20aef31c663ad8bb514284dbd468020
Hydrogen <34059898+thehydrogen@users.noreply.github.com>
2023-02-02 15:40:10 +0100

    add methods to entity interfaces

--- ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerEntity.java
+++ ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerEntity.java
@@ -3,4 +3,7 @@ package ac.artemis.packet.wrapper.server;
 import ac.artemis.packet.wrapper.PacketServer;
 
 public interface PacketPlayServerEntity extends PacketServer {
+
+    int getEntityId();
+    boolean isOnGround();
 }
--- ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerEntityRelMove.java
+++ ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerEntityRelMove.java
@@ -3,4 +3,8 @@ package ac.artemis.packet.wrapper.server;
 import ac.artemis.packet.wrapper.PacketServer;
 
 public interface PacketPlayServerEntityRelMove extends PacketPlayServerEntity {
+
+    short getX();
+    short getY();
+    short getZ();
 }
