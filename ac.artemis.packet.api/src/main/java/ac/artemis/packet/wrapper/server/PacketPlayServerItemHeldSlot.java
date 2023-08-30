43a08b9956cf734236982769ab7f6eae3e5f5c38
Tecnio <50290580+Tecnio@users.noreply.github.com>
2023-08-31 00:08:45 +0300

    Held item slot fix

--- ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerItemHeldSlot.java
+++ ac.artemis.packet.api/src/main/java/ac/artemis/packet/wrapper/server/PacketPlayServerItemHeldSlot.java
@@ -3,5 +3,5 @@ package ac.artemis.packet.wrapper.server;
 import ac.artemis.packet.wrapper.PacketServer;
 
 public interface PacketPlayServerItemHeldSlot extends PacketServer {
-    short getSlot();
+    byte getSlot();
 }
